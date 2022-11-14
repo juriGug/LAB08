cd59c4b33bf8a0048db875ff3c2d9c428df73f5a
Juri Guglielmi - juri.guglielmi@studio.unibo.it <juri.guglielmi@studio.unibo.it>
2022-11-14 13:03:45 +0100

    inizio secondo esercizio

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,46 @@
 package it.unibo.mvc;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    File currentFile;
+    
+    public Controller(final File file){
+        this.currentFile = file;
+    }
 
+    public Controller(){
+        this.currentFile = new File("output.txt");
+    }
+
+    public void SetCurrentFile(final File file){
+        this.currentFile = file;
+    }
+
+    public File getFile(){
+        return this.currentFile;
+    }
+
+    public String getPath(){
+        return this.currentFile.getAbsolutePath();
+    }
+
+    public void WriteOnFIle(final String s) throws IOException{
+        try(final DataOutputStream objWrite = 
+            new DataOutputStream(new BufferedOutputStream(
+                new FileOutputStream(currentFile))))
+        {
+            objWrite.writeUTF(s);;
+        }
+        catch(IOException ex){
+            throw new IOException(ex);
+        }
+    }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +13,29 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame;
+
+    public SimpleGUI(){
+        frame = new JFrame();
+        frame.setTitle("My first Java graphical interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        final JTextArea area = new JTextArea();
+        final JButton save = new JButton();
+        panel.add(area, BorderLayout.AFTER_LAST_LINE);
+        panel.add(save, BorderLayout.SOUTH);
+
+        final Controller c = new Controller();
+
+        
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI();
+     }
 
 }
